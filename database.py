76bfcd622dbfa64836cc6388de113e6a9b040963
Samia Errabite <samiaerrabite@Air-de-Samia.lan>
2026-06-03 22:09:27 +0200

    log

--- database.py
+++ database.py
@@ -1,12 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, create_engine, Session
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost:8889/healthnorthE6"
+load_dotenv()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME")
 
-engine = create_engine(DATABASE_URL, echo=True) 
-def get_session(): 
-    with Session(engine) as session: yield session
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
